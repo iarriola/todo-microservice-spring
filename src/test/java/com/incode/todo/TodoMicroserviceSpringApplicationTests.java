1105e74517d2379e58820eaf4910c49f0b3240dc
Ivan Arriola Romero <iarriolar@gmail.com>
2021-01-18 23:06:42 -0600

    Init repo

---
+++ src/test/java/com/incode/todo/TodoMicroserviceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incode.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMicroserviceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
